7566e62946ef18c28a53eaeb4e7b08e2a438d39f
sin-45 <sin45.0403@gmail.com>
2025-11-04 02:14:18 +0900

    Rust_Aを記述

--- ABC_427/Rust/A/src/main.rs
+++ ABC_427/Rust/A/src/main.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+use std::collections::HashSet; // set -> hash
+use std::collections::HashMap; // 連想配列 -> hash
+use std::collections::BinaryHeap; // priority_queue -> 二分ヒープ
+use std::collections::BTreeSet; // set -> 平衡二分木 (log n)
+use std::collections::BTreeMap; // 連想配列 -> 平行二分木 (log n)
+use std::collections::VecDeque; // Deque(queue)
+use std::collections::LinkedList; // list -> あまり便利ではないかも
+use std::cmp::Reverse;
+use std::cmp::min;
+use std::cmp::max;
+use itertools::Itertools;
+use itertools::iproduct;
+
 fn main() {
-    println!("Hello, world!");
+    input! {
+        s: u16,
+        a: u16,
+        b: u16,
+        x: u16,
+    }
+    let cnt: u16 = x / (a+b);
+    let plus: u16 = min(x % (a+b), a);
+    println!("{}", cnt * a * s + plus * s);
 }
