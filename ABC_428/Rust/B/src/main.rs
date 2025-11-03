a0d7aef5a6fb28d73bf9e4d1d5d3a1c8bae2a216
sin-45 <sin45.0403@gmail.com>
2025-11-04 02:45:29 +0900

    ABC_428 Rustを記述 間違えてABC427のところに記述してしまっていたので今後変更する

--- ABC_428/Rust/B/src/main.rs
+++ ABC_428/Rust/B/src/main.rs
@@ -1,3 +1,34 @@
+use proconio::input;
+use std::collections::HashSet; // set -> hash
+use std::collections::HashMap; // 連想配列 -> hash
+use std::collections::BinaryHeap; // priority_queue -> 二分ヒープ
+use std::collections::BTreeSet; // set -> 平衡二分木 (log n)
+use std::collections::BTreeMap; // 連想配列 -> 平行二分木 (log n)
+use std::collections::VecDeque; // Deque(queue)
+use std::collections::LinkedList; // list -> あまり便利ではないかも
+use std::cmp::Reverse;
+use std::cmp::min;
+use std::cmp::max;
+use itertools::Itertools;
+use itertools::iproduct;
+
 fn main() {
-    println!("Hello, world!");
+    input! {
+        n: usize,
+        k: usize,
+        s: String,
+    }
+    let mut hash: BTreeMap<&str, u16> = BTreeMap::new();
+    for i in 0..n-k+1 {
+        let slice: &str = &s[i..i+k];
+        *hash.entry(slice).or_insert(0) += 1;
+    }
+    let max: u16 = *hash.values().max().unwrap();
+    println!("{}", max);
+    for (key, value) in &hash {
+        if *value == max {
+            print!("{} ", key);
+        }
+    }
+    println!("");
 }
