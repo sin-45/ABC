2791fd6026e824baa7ae3035427527c2ee591b24
sin-45 <sin45.0403@gmail.com>
2025-11-05 01:01:36 +0900

    Rust_Cを記述

--- ABC_427/Rust/C/src/main.rs
+++ ABC_427/Rust/C/src/main.rs
@@ -1,1 +1,32 @@
-z
+use proconio::input;
+use std::collections::HashSet; // set -> hash
+use std::collections::HashMap; // 連想配列 -> hash
+use std::collections::BinaryHeap; // priority_queue -> 二分ヒープ
+use std::collections::BTreeSet; // set -> 平衡二分木 (log n)
+use std::collections::BTreeMap; // 連想配列 -> 平行二分木 (log n)
+use std::collections::VecDeque; // Deque(queue)
+use std::collections::LinkedList; // list -> あまり便利ではないかも
+use std::cmp::Reverse;
+use std::cmp::min;
+use std::cmp::max;
+use itertools::Itertools;
+use itertools::iproduct;
+
+fn main() {
+    input! {
+        n: usize,
+        m: usize,
+        eg: [(u16, u16); m],
+    }
+    let mut ans: u16 = m as u16;
+    for i in 0..2_i32.pow(n as u32) {
+        let mut cnt: u16 = 0;
+        for (u, v) in &eg {
+            if ((i >> u) & 1) == ((i >> v) & 1) {
+                cnt += 1;
+            }
+        }
+        ans = min(ans, cnt);
+    }
+    println!("{}", ans);
+}
