027c738bc278923d5778cee6d302dc7bfd6b3847
sin-45 <sin45.0403@gmail.com>
2025-12-01 23:45:01 +0900

    RustでAを記述

--- ABC_433/Rust/A/src/main.rs
+++ ABC_433/Rust/A/src/main.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+use std::collections::HashSet; // set -> hash
+use std::collections::HashMap; // 連想配列 -> hash
+use std::collections::BinaryHeap; // priority_queue -> 二分ヒープ
+use std::collections::BTreeSet; // set -> 平衡二分木 (log n)
+use std::collections::BTreeMap; // 連想配列 -> 平行二分木 (log n)
+use std::collections::VecDeque; // Deque(queue)
+use std::collections::LinkedList; // list -> あまり便利ではないかも
+use std::cmp::Reverse;
+use std::cmp::min;
+use std::cmp::max;
+use itertools::Itertools;
+use itertools::iproduct;
+
 fn main() {
-    println!("Hello, world!");
+    input! {
+        x: u64,
+        y: u64,
+        z: u64,
+    }
+    let mut ans: &str = "No";
+    for i in 0..1000 {
+        if x+i == (y+i) * z {
+            ans = "Yes";
+            break;
+        }
+    }
+    println!("{}", ans);
 }
