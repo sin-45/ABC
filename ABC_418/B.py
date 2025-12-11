5f01b7c2779a84517b2fc9a68a46d06013c3e059
sin-45 <sin45.0403@gmail.com>
2025-12-12 00:05:54 +0900

    PythonでBを記述

--- ABC_418/B.py
+++ ABC_418/B.py
@@ -1,7 +1,7 @@
 s = input()
 ans = 0
-for i in range(len(s)-3):
-    for j in range(i+3, len(s)):
+for i in range(len(s)-2):
+    for j in range(i+2, len(s)):
         cnt = s[i:j+1].count("t")
         ans = max(ans, (cnt-2) / (j-i-1))
 
